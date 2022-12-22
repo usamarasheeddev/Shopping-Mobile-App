ef616e351ad8f2efa7f46002c86318f6dd000ea8
Usama Rasheed <65440307+usama2188@users.noreply.github.com>
2022-12-22 13:49:48 +0500

    login

---
+++ src/Components/Inputs/Input.js
@@ -0,0 +1,33 @@
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function Input(props) {
+    const { placeholder, label, error } = props
+    return (
+        <>
+            <View style={{ margin: 12 }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 2, paddingHorizontal: 5 }}>
+                    <Text style={{ fontWeight: 'bold' }}>{label}</Text>
+                    {error ? <Text style={{ color: 'red', fontSize: 12 }}>{error}</Text> : null}
+                </View>
+                <TextInput {...props} placeholder={placeholder} style={styles.input} />
+
+            </View>
+
+
+        </>
+    )
+}
+const styles = StyleSheet.create({
+
+    input: {
+        borderWidth: 0,
+        borderColor: 'grey',
+        height: 37,
+        borderRadius: 5,
+        padding: 4,
+        border: 'none',
+        // borderBottomWidth: 1,
+        borderBottomWidth: 1,
+    }
+})
